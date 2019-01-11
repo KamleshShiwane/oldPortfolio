068c4db215d3759dfbbb4bc6f042ba7e908b5bd5
Luke <ReventonC@users.noreply.github.com>
2019-01-10 18:52:56 -0800

    update cv page

--- src/components/CV.vue
+++ src/components/CV.vue
@@ -1,19 +1,58 @@
 <template>
-    <div>
-        CV
-    </div>
+  <div class="content">
+    <h1>CV</h1>
+    <p>
+      You can get my latest downloadable resume
+      <a href='https://drive.google.com/file/d/1u4-hf1CmcckHMNUspBnzd9cW0JhWjHF4/view?usp=sharing' target="_blank">here</a>. (Updated 12/27/2018)
+    </p>
+    <h2>Education</h2>
+    <p>B.S. @ UCSD</p>
+    <h2>Project Experience</h2>
+    <ul>
+        <li>UCSD.github.io</li>
+        <li>COGS18.github.io</li>
+        <li>Pantry</li>
+        <li>Data Science Project on SAN DIEGO ROAD &amp; ACCIDENT|</li>
+        <li>Racconal</li>
+        <li>JSA Event Planner</li>
+        <li>Ray tracer</li>
+    </ul>
+    <h2>Teaching Experience</h2>
+    <ul>
+        <li>Winter 2019 COGS 108 Instructional Assistant</li>
+        <li>Fall 2018 COGS 18 Instructional Assistant</li>
+    </ul>
+    <h2>Skills &amp; Tools</h2>
+  </div>
 </template>
 
 <script>
 /* eslint-disable */
 export default {
-  data () {
-    return {
-    }
-
+  data() {
+    return {};
   }
-}
+};
 </script>
 
 <style scoped lang="scss">
+* {
+  text-align: left;
+}
+.content {
+  margin: 50px auto;
+  width: 70%;
+}
+
+h1 {
+    font-size: 3em;
+}
+h2 {
+    margin-top: 20px;
+    font-size: 2em;
+}
+
+p {
+    font-size: 1.5em;
+}
 </style>
