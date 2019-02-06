c618f399a899e1845a83d29676e3c0d143f63313
Luke <ReventonC@users.noreply.github.com>
2019-02-05 21:51:31 -0800

    update cv

--- src/components/CV.vue
+++ src/components/CV.vue
@@ -3,25 +3,131 @@
     <h1>CV</h1>
     <p>
       You can get my latest downloadable resume
-      <a href='https://drive.google.com/file/d/1u4-hf1CmcckHMNUspBnzd9cW0JhWjHF4/view?usp=sharing' target="_blank">here</a>. (Updated 12/27/2018)
+      <a
+        href="https://drive.google.com/file/d/1jnLn4ecyjZdQ0wcK4KJPjudKZevos4Te/view?usp=sharing"
+        target="_blank"
+      >here</a>. (Updated 02/05/2019)
     </p>
-    <h2>Education</h2>
-    <p>B.S. @ UCSD</p>
-    <h2>Project Experience</h2>
-    <ul>
-        <li>UCSD.github.io</li>
-        <li>COGS18.github.io</li>
-        <li>Pantry</li>
-        <li>Data Science Project on SAN DIEGO ROAD &amp; ACCIDENT|</li>
-        <li>Racconal</li>
-        <li>JSA Event Planner</li>
-        <li>Ray tracer</li>
-    </ul>
-    <h2>Teaching Experience</h2>
-    <ul>
-        <li>Winter 2019 COGS 108 Instructional Assistant</li>
-        <li>Fall 2018 COGS 18 Instructional Assistant</li>
-    </ul>
+    <div class="education">
+      <h2>Education</h2>
+      <table>
+        <tr>
+          <td class="date">2015.09 - Current</td>
+          <td class="degree">
+            <b>B.S. in Cognitive Science (Expected June 2019)</b>
+          </td>
+        </tr>
+        <tr>
+          <td></td>
+          <td class="location">
+            <i>University of California, San Diego</i>, La Jolla, United States
+          </td>
+        </tr>
+        <tr>
+          <td></td>
+          <td
+            class="major"
+          >Major: Cognitive Science / Human-Computer Interaction; Minor: Computer Science</td>
+        </tr>
+      </table>
+    </div>
+    <div class="experience">
+      <h2>Relative Experience</h2>
+      <table>
+        <tr>
+          <td class="date">2019.02 - Current</td>
+          <td class="name">
+            <b>UI/UX Engineer Internship</b>
+          </td>
+          <td class="location">
+            <i>Family Proud</i>, La Jolla
+          </td>
+        </tr>
+        <tr>
+          <td class="date">2018.04 - 2018.06</td>
+          <td class="name">
+            <router-link active-class="active" to="/JSA" exact>
+              <b>Pantry</b>
+            </router-link>
+          </td>
+          <td class="location">
+            <i>University of California, San Diego</i>, La Jolla
+          </td>
+        </tr>
+        <tr>
+          <td class="date">2018.03 - Current</td>
+          <td class="name">
+            <router-link active-class="active" to="/JSA" exact>
+              <b>Webmaster</b>
+            </router-link>
+          </td>
+          <td class="location">
+            <i>UCSD Japanese Student Association</i>, La Jolla
+          </td>
+        </tr>
+        <tr>
+          <td class="date">2018.01 - 2018.03</td>
+          <td class="name">
+            <router-link active-class="active" to="/JSA" exact>
+              <b>Raccoonal</b>
+            </router-link>
+          </td>
+          <td class="location">
+            <i>University of California, San Diego</i>, La Jolla
+          </td>
+        </tr>
+        <tr>
+          <td class="date">2018.01 - 2018.03</td>
+          <td class="name">
+            <b>Data Science Project on San Diego road condition &amp; accident</b>
+          </td>
+          <td class="location">
+            <i>University of California, San Diego</i>, La Jolla
+          </td>
+        </tr>
+        <tr>
+          <td class="date">2018.01 - 2018.03</td>
+          <td class="name">
+            <b>JSA Event Planner</b>
+          </td>
+          <td class="location">
+            <i>University of California, San Diego</i>, La Jolla
+          </td>
+        </tr>
+        <tr>
+          <td class="date">2017.01 - 2017.03</td>
+          <td class="name">
+            <b>Ray tracer</b>
+          </td>
+          <td class="location">
+            <i>University of California, San Diego</i>, La Jolla
+          </td>
+        </tr>
+      </table>
+    </div>
+    <div class="teach">
+      <h2>Teaching Experience</h2>
+      <table>
+        <tr>
+          <td class="date">2019.01 - Current</td>
+          <td class="name">
+            <b>Instructional Assistant, COGS 108: Data Science in Practice</b>
+          </td>
+          <td class="location">
+            <i>University of California, San Diego</i>, La Jolla
+          </td>
+        </tr>
+        <tr>
+          <td class="date">2018.09 - 2018.12</td>
+          <td class="name">
+            <b>Instructional Assistant, COGS 18: Introduction to Python</b>
+          </td>
+          <td class="location">
+            <i>University of California, San Diego</i>, La Jolla
+          </td>
+        </tr>
+      </table>
+    </div>
     <h2>Skills &amp; Tools</h2>
   </div>
 </template>
@@ -45,14 +151,48 @@ export default {
 }
 
 h1 {
-    font-size: 3em;
+  font-size: 3em;
 }
 h2 {
-    margin-top: 20px;
-    font-size: 2em;
+  margin-top: 20px;
+  font-size: 2em;
 }
 
 p {
-    font-size: 1.5em;
+  font-size: 1.5em;
+}
+
+table {
+    border-spacing: 10px;
+}
+
+.education {
+  td {
+    font-size: 1.2em;
+  }
+
+  .date {
+    font-size: 1em;
+  }
+}
+
+.experience {
+  td {
+    font-size: 1.2em;
+  }
+
+  .date {
+    font-size: 1em;
+  }
+}
+
+.teach {
+  td {
+    font-size: 1.2em;
+  }
+
+  .date {
+    font-size: 1em;
+  }
 }
 </style>
